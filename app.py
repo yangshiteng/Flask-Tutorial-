b9df1810c39cb1a092da4fda634f8f71ad5a557c
Shiteng Yang <60442877+yangshiteng@users.noreply.github.com>
2022-02-03 18:27:07 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
